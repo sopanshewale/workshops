101b51874bb60e38ff7e3ea3b6f54ce71aa0cab4
Sopan Shewale <sopan.shewale@gmail.com>
2017-07-01 20:55:37 +0530

    Adding Templates to the Application

--- application/django_workshop/dravate/views.py
+++ application/django_workshop/dravate/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Dravate Says Hello World!")
+    #return HttpResponse("Dravate Says Hello World!")
+    context_d = {'boldmessage': "I am actually bold fond"}
+    return render(request, 'dravate/index.html', context_d)
 
